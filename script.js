020d6260c4bf3b49b64cc216a4c7c1af21a9feac
Shroomy Ralsei <142345364+ShroomyRalsei@users.noreply.github.com>
2023-12-10 12:03:37 -0400

    fix:Fix random ad img path

--- script.js
+++ script.js
@@ -13,19 +13,19 @@ function randomNumberGenerator() {
 function createRandomAd() {
     randomNumberGenerator();
     if (randomNumber <= 0.2) {
-        randomAd.innerHTML = `<img class="random-ad" src="./images/ad1.jpg">`
+        randomAd.innerHTML = `<img class="random-ad" src="file:///home/ubuntujs/repos/spanish-newspaper-school-project/images/ad1.jpg">`
     }
     else if (randomNumber <= 0.4 && randomNumber > 0.2) {
-        randomAd.innerHTML = `<img class="random-ad" src="./images/ad2.jpg">`
+        randomAd.innerHTML = `<img class="random-ad" src="file:///home/ubuntujs/repos/spanish-newspaper-school-project/images/ad2.jpg">`
     }
     else if (randomNumber <= 0.6 && randomNumber > 0.4) {
-        randomAd.innerHTML = `<img class="random-ad" src="./images/ad3.jpg">`
+        randomAd.innerHTML = `<img class="random-ad" src="file:///home/ubuntujs/repos/spanish-newspaper-school-project/images/ad3.jpg">`
     }
     else if (randomNumber <= 0.8 && randomNumber > 0.6) {
-        randomAd.innerHTML = `<img class="random-ad" src="./images/ad4.jpg">`
+        randomAd.innerHTML = `<img class="random-ad" src="file:///home/ubuntujs/repos/spanish-newspaper-school-project/images/ad4.jpg">`
     }
     else {
-        randomAd.innerHTML = `<img class="random-ad" src="./images/ad5.jpeg">`
+        randomAd.innerHTML = `<img class="random-ad" src="file:///home/ubuntujs/repos/spanish-newspaper-school-project/images/ad5.jpeg">`
     }
 };
 
